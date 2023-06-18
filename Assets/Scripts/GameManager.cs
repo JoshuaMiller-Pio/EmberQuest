70fb5a2b28efc39af18af38debfc3fd07398013d
WinnieThePio <81216801+JoshuaMiller-Pio@users.noreply.github.com>
2023-06-18 13:53:11 +0200

    added flowers and started updating enemies

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
     // Start is called before the first frame update
-    private string PlayerName;
- 
+    private string _PlayerName;
+    public bool HasHerbs;
+
+    private int _NumOfHerbs;
 
     #region Properties
     public string playerName
     {
-        set { PlayerName = value; }
-        get { return PlayerName; }
+        set { _PlayerName = value; }
+        get { return _PlayerName; }
+    }
+    public int numOfHerbs
+    {
+        set { _NumOfHerbs = value; }
+        get { return _NumOfHerbs;}
     }
 
     #endregion
@@ -25,6 +33,10 @@ public class GameManager : Singleton<GameManager>
     // Update is called once per frame
     void Update()
     {
+        if(numOfHerbs == 2 && !HasHerbs)
+        {
+            HasHerbs = true;
+        }
     }
 
     public void PlayerDead()
---
+++ Assets/Scripts/world/PlantPickup.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantPickup : MonoBehaviour
+{
+    bool inRange = false;
+    bool hasRun = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (inRange && !hasRun)
+        {
+            pickup();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("range");
+        if(collision.tag == "Player")
+        {
+
+         inRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+
+            inRange = false;
+        }
+    }
+
+    private void pickup()
+    {
+            Debug.Log("pick");
+        if (Input.GetKeyDown("e"))
+        {
+            hasRun = true;
+            GameManager.Instance.numOfHerbs++;
+            Destroy(gameObject);
+        }
+    }
+}
